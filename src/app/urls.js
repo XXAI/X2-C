f6151f1e2cbf12965ecb64942755cb01d6bb2217
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2016-08-04 21:12:02 -0500

    Correcion en urls.js para usar servidor local

---
+++ src/app/urls.js
@@ -0,0 +1,7 @@
+(function(){
+	'use strict';
+	angular.module('App').constant('URLS', {
+    	BASE: 'http://localhost/desabasto-api/public',
+    	BASE_API: 'http://localhost/desabasto-api/public'
+   	});	
+})();
