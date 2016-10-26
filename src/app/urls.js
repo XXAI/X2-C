7c3211c8c286fec84a7b1bdc9251c63b26beabdf
Mario Alberto Cabrera Alfaro <maca.15c@gmail.com>
2016-10-26 11:05:58 -0500

    correcion de rutas en urls.js

--- src/app/urls.js
+++ src/app/urls.js
@@ -1,7 +1,7 @@
 (function(){
     'use strict';
     angular.module('App').constant('URLS', {
-        BASE: 'http://localhost/control_desabasto_api/control-de-desabasto-unidades-medicas-api/public/',
-        BASE_API: 'http://localhost/control_desabasto_api/control-de-desabasto-unidades-medicas-api/public/'
+        BASE: 'http://localhost/salud/samm-unidades-api/public',
+    	BASE_API: 'http://localhost/salud/samm-unidades-api/public'
     });
 })();
