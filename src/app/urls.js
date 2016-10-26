ceadf9c34635e71ac8e81f75d7d2bcce39d7c724
Mario Alberto Villatoro <ing.mario.villatoro@gmail.com>
2016-10-25 21:57:31 -0500

    Actualizacion de modulo de unidades

--- src/app/urls.js
+++ src/app/urls.js
@@ -1,7 +1,7 @@
 (function(){
-	'use strict';
-	angular.module('App').constant('URLS', {
-    	BASE: 'http://localhost/desabasto-api/public',
-    	BASE_API: 'http://localhost/desabasto-api/public'
-   	});	
+    'use strict';
+    angular.module('App').constant('URLS', {
+        BASE: 'http://localhost/control_desabasto_api/control-de-desabasto-unidades-medicas-api/public/',
+        BASE_API: 'http://localhost/control_desabasto_api/control-de-desabasto-unidades-medicas-api/public/'
+    });
 })();
